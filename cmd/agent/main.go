d20166b788282661405c80e110e1b1ff87b67ce6
Gorums <acksecurity@hotmail.com>
2020-12-07 10:42:10 -0500

    Create a services

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"time"
+	"log"
+	"math/rand"
+	"fmt"
+	"github.com/kardianos/service"
+)
+
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+	// Do work here
+	// infinite print loop
+        for {
+          fmt.Println("Hello, World!")
+
+          // wait random number of milliseconds
+          Nsecs := rand.Intn(3000)
+          time.Sleep(time.Millisecond * time.Duration(Nsecs))
+        }	
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	svcConfig := &service.Config{
+		Name:        "GoServiceTest",
+		DisplayName: "Go Service Test",
+		Description: "This is a test Go service.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
